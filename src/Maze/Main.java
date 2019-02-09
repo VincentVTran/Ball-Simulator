72e96ab17cd4d40673a50cdcb865a97ccbff515a
Vincent Tran <VincentTran197120@gmail.com>
2019-02-08 23:18:26 -0600

    Rebuild

--- src/Maze/Main.java
+++ src/Maze/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("automatic_movement.fxml"));
-        primaryStage.setTitle("Maze");
+        primaryStage.setTitle("MazeNN");
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
